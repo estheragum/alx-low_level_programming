aba8da84c32dd80b9d363d5dfe4194275697e3eb
estheragum <mimidooocelina@gmail.com>
2022-09-09 13:27:20 -0700

    my 6th task commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
 /**
+ * main - Prints the sizeof many var types
  *
- *  * main - Prints sized of characters
- *
- *   *
- *
- *    *Return: Always 0 (success)
- *
- *     */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	char a;
 
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	int b;
 
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	long int c;
 
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	long long int d;
 
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 
 	return (0);
 
