e5d8d3042661ed35bf22be1f2bf59adc93378e2c
estheragum <mimidooocelina@gmail.com>
2022-09-10 18:37:43 -0700

    4th upgraded 5th task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 
-  /**
-   *
-   * main - Entry point
-   * My hello, print task
-   * Return: return 1 after program execution
-   */
+ /**
+ * main - Entry point
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
 
- int main(void)
 
- {
+int main(void)
 
-    Printf("with proper grammar, but the outcome is a piece of art,\n");
-    Printf("Size of an int: %d byete(s) \n", 
-	return (0);
+{
 
- }
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
+}
