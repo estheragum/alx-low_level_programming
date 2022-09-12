ddb4fcfe1e3b148600687c93a55aae0f9ad07d0b
estheragum <mimidooocelina@gmail.com>
2022-09-12 10:46:35 -0700

    my 3rd 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entyr point
+* main - Prints the numbers from 00 to 99, numbers seperated by
+ * a comma followed by a space, in ascending order.
+ *
+ * main - Entry point
  * Description: prints two digits combination
  * Return: Always 0 (success)
  */
 int main(void)
 {
+int digit1, digit2;
 int c, i;
 
+for (digit2 = 0; digit2 <= 9; digit2++)
 for (c = '0'; c <= '9'; c++)
 {
+for (digit2 = 0; digit2 <= 9; digit2++)
 for (i = '0'; i <= '9'; i++)
 {
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 if (c < i)
 {
 putchar(c);
 putchar(i);
+}
+}
+}
+}
 
-if (c != '8' || (c == '8' && i != '9'))
-}
-}
-}
-}
 putchar('\n');
+
 return (0);
 }
