3be4cfd6b120d5e5a6a7092a26bae032c3f67cf2
estheragum <mimidooocelina@gmail.com>
2022-09-14 01:37:10 -0700

    task -print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- *  print_alphabet - prints alphabet
+ *  print_alphabet_x10 - prints alphabet 10 times
  *
  *  Return: always 0;
  *
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char i;
+char h;
 
+for (h = 0; h <= 9; h++)
+{
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
+}
 }
