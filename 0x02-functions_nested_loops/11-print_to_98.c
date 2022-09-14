bc3ef46cae265cba97ee3e9f5f66562a31386b1e
estheragum <mimidooocelina@gmail.com>
2022-09-14 09:19:40 -0700

    task 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,14 +1,18 @@
 #include "main.h"
 
 /**
- * add - adding it up
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of both integers
+ * add - adds two integers
+ * @n1: integer 1
+ * @n2: integer 2
+ *  Description: give two integers, returns sum
+ *  Return: sum
  */
 
-int add(int a, int b)
+int add(int n1, int n2)
 {
-return (a + b);
+int sum;
+
+sum = n1 + n2;
+
+return (sum);
 }
