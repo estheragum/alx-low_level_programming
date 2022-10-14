4d82665f6cb61c502741f8eeb08f18e69522d9f9
estheragum <mimidooocelina@gmail.com>
2022-10-14 11:00:20 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: The array
+ * @size: The number of elements in the array
+ * @cmp: a pointer to tge function to be used to compare variables
+ * Return: 0 or -1 depending on condition
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int x, i;
+
+if (size > 0 && array && cmp)
+{
+x = cmp(array[i]);
+if (x)
+break;
+}
+if (i < size)
+return (i);
+}
+return (-1);
+}
